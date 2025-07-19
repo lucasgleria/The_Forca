c102c4733684e98e6cd333a399def26b93216916
Ana Paula Monteiro <ana.monteiro1864@gmail.com>
2025-07-19 14:58:31 -0300

    Endpoints da API

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from uuid import uuid4
 from fastapi.middleware.cors import CORSMiddleware
+from game_logic import GameSession
+from word_manager import get_random_word
 
 app = FastAPI()
 
@@ -17,6 +20,50 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/api/hello")
-def hello():
-    return {"message": "Hello from FastAPI!"}
+
+sessions = {}
+
+@app.post("/api/game/start")
+async def start_game(request: Request):
+    data = await request.json()
+    difficulty = data.get("difficulty", "easy")
+
+    word = get_random_word(difficulty)
+    session_id = str(uuid4())
+
+    game_session = GameSession(word)
+    sessions[session_id] = game_session
+
+    return{
+        "session_id": session_id,
+        "game_state": game_session.get_game_state()
+    }
+
+
+@app.post("/api/game/guess")
+async def make_guess(request: Request):
+    data = await request.json()
+        session_id = data.get("session_id")
+        letter = data.get("letter")
+    
+    if session_id not in sessions:
+        return {"error": "Sessão inválida."}
+
+    game_session = sessions[session_id]
+
+    if letter in game_session.guessed_letters:
+        return{"error": "Letra já tentada.", "game_state": game_session.get_game_state()}
+
+    game_session.make_guess(letter)
+
+    return{"game_state": game_session.get_game_state()}
+
+
+@app.get("/api/game/status/{session_id}")
+async def get_status(session_id: str):
+    game_session = sessions.get(session_id)
+
+    if not game_session:
+        return{"error": "Sessão não encontrada."}
+
+    return {"game_state": game_session.get_name_state()}
